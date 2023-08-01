55e995d7eb5d14ecfea80c90d44108189ac295a0
HunterStag <hunter.stag@gmail.com>
2023-08-01 18:56:08 -0400

    Adds skeleton of web game

---
+++ trivialogic.js
@@ -0,0 +1,79 @@
+
+let questions = [
+    {
+        question: "Which film has the longest runtime?",
+        options: ["Magnolia", "Pi", "Pulp Fiction", "The Lion King"],
+        answer: 0
+    },
+    {
+        question: "Who won the Oscar for best supporting actor in 1995?",
+        options: ["Anthony Hopkins", "Val Kilmer", "Kevin Spacey" , "Guy Pearce"],
+        answer: 2
+
+    },
+    {
+        question: "Who was the top-billed actor in the 1990s mob epic Goodfellas?",
+        options: ["Ray Liotta", "Robert De Niro", "Joe Pesci" , "Lorraine Bracco"],
+        answer: 1
+
+    },
+    {
+        question: "Who co-wrote Pulp Fiction with Director Quentin Tarantino?",
+        options: ["Aaron Sorkin", "Todd Solondz", "David Fincher" , "Roger Avary"],
+        answer: 3
+
+    },
+    {
+        question: "Which 90s romantic comedy featured the song 'Kiss Me' by the band Sixpence None The Richer?",
+        options: ["Clueless", "She's All That", "American Pie" , "Sixteen Candles"],
+        answer: 1
+    },
+    {
+        question: "Which 90s Indie Film featured an ensemble cast including Phillip Seymour Hoffman, Dylan Baker, Jane Adams, Marla Maples, Jon Lovitz and Lara Flynn Boyle?",
+        options: ["Happiness", "Natural Born Killers", "Boogie Nights", "Dazed and Confused",],
+        answer: 0
+    }
+
+];
+
+let currentQuestion = 0;
+let score = 0;
+let answered = false;  // new
+
+function showQuestion() {
+    let q = questions[currentQuestion];
+    document.getElementById('question').textContent = q.question;
+    let options = document.getElementById('options');
+    options.innerHTML = '';
+    for(let i=0; i<q.options.length; i++) {
+        let div = document.createElement('div');
+        div.textContent = q.options[i];
+        div.classList.add('option');
+        div.onclick = function() {
+            if(answered) { return; }  // new
+            answered = true;  // new
+            document.getElementById('next').disabled = false;
+            let feedback = document.getElementById('feedback');
+            if(i === q.answer) {
+                score++;
+                feedback.textContent = "Correct!";  // new
+            } else {
+                feedback.textContent = "Incorrect. The correct answer was: " + q.options[q.answer];  // new
+            }
+        };
+        options.appendChild(div);
+    }
+}
+
+document.getElementById('next').onclick = function() {
+    currentQuestion++;
+    answered = false;  // new
+    document.getElementById('feedback').textContent = '';  // new
+    if(currentQuestion >= questions.length) {
+        document.getElementById('score').textContent = 'Score: ' + score;
+    } else {
+        showQuestion();
+    }
+};
+
+showQuestion();
